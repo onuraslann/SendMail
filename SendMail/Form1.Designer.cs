071a6a34a3996d27750ae6bd0537d061172c9ee1
Onuraslann <theeaslann1@gmail.com>
2022-06-26 21:36:09 +0300

    Add project files.

---
+++ SendMail/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace SendMail
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.TxtWho = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(458, 461);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SendMail";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(58, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Who : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(58, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Title : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(54, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Message : ";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(183, 114);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(281, 27);
+            this.txtTitle.TabIndex = 4;
+            // 
+            // TxtWho
+            // 
+            this.TxtWho.Location = new System.Drawing.Point(183, 164);
+            this.TxtWho.Name = "TxtWho";
+            this.TxtWho.Size = new System.Drawing.Size(281, 27);
+            this.TxtWho.TabIndex = 5;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(183, 228);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(281, 193);
+            this.txtMessage.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1175, 558);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.TxtWho);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox TxtWho;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ SendMail/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+namespace SendMail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MailMessage mesajım = new MailMessage();
+            SmtpClient istemci = new SmtpClient();
+            istemci.UseDefaultCredentials = false;
+            istemci.Host = "smtp.outlook.com"; //sunucu bilgileri //smtp.gmail.com
+            istemci.Port = 587;
+            istemci.Credentials = new System.Net.NetworkCredential("Theonur212121@hotmail.com", "onur2121"); //kimlik bilgileri 
+            istemci.DeliveryMethod = SmtpDeliveryMethod.Network;
+            istemci.EnableSsl = true;
+            mesajım.To.Add(TxtWho.Text);//maili nereye gönderecez
+            mesajım.From = new MailAddress("Theonur212121@hotmail.com"); // mesajı kime göndercez
+            mesajım.Subject = txtTitle.Text;//title
+            mesajım.Body = txtMessage.Text;
+            istemci.Send(mesajım);
+
+
+        }
+    }
+}
